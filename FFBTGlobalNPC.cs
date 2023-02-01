1912bd78e5d18c17db88073f9a68c68cf8536411
Sky-Meteor <2746501855@qq.com>
2023-02-01 23:44:43 +0800

    可以跑了

--- FFBTGlobalNPC.cs
+++ FFBTGlobalNPC.cs
@@ -1,4 +1,6 @@
 ﻿using FargowiltasSouls.NPCs.Challengers;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.DataStructures;
@@ -9,19 +11,51 @@ namespace FurgosFargoBossTimer
 {
     public class FFBTGlobalNPC : GlobalNPC
     {
+        public override bool InstancePerEntity => true;
+        public int Phase;
+        public Dictionary<int, int> BossPhaseTimer = new Dictionary<int, int>();
+
         public override void OnSpawn(NPC npc, IEntitySource source)
         {
-            base.OnSpawn(npc, source);
+            if (!BossPhases.ContainsKey(npc.type))
+                return;
+            int phaseCount = BossPhases[npc.type].Keys.Count;
+            for (int i = 1; i <= phaseCount; i++)
+                BossPhaseTimer.Add(i, 0);
+            Phase = 1;
         }
 
         public override void PostAI(NPC npc)
         {
-            base.PostAI(npc);
+            if (!BossPhases.ContainsKey(npc.type))
+                return;
+            BossPhaseTimer[Phase]++;
+            if (npc.life <= npc.lifeMax * BossPhases[npc.type][Phase])
+                Phase++;
         }
 
         public override void OnKill(NPC npc)
         {
-            base.OnKill(npc);
+            if (!BossPhases.ContainsKey(npc.type))
+                return;
+            Main.NewText(Lang.GetNPCNameValue(npc.type) + "：", Color.Lerp(Color.YellowGreen, Color.Red, 0.65f));
+            foreach (KeyValuePair<int, int> phaseTimer in BossPhaseTimer)
+            {
+                float second = (float)Math.Round((double)phaseTimer.Value / 60, 2);
+                int minute = (int)(second / 60);
+                string secondDisplay = second.ToString();
+                if (!secondDisplay.Contains("."))
+                    secondDisplay += ".00";
+                string[] splitSecond = secondDisplay.Split(".");
+                if (splitSecond[0].Length == 1)
+                    secondDisplay = "0" + secondDisplay;
+                if (splitSecond[1].Length == 1)
+                    secondDisplay += "0";
+                string minuteDisplay = minute.ToString();
+                if (minuteDisplay.Length == 1)
+                    minuteDisplay = "0" + minuteDisplay;
+                Main.NewText($"第{phaseTimer.Key}阶段：{minuteDisplay}:{secondDisplay}", Color.Lerp(Color.Lime, Color.LightBlue, 0.5f));
+            }
         }
 
         public readonly static Dictionary<int, Dictionary<int, float>> BossPhases = new Dictionary<int, Dictionary<int, float>>()
